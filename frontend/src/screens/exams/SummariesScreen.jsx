48b7373c002cb092149d8133188fba126bc3a5ce
Aizen <acristyano22@gmail.com>
2024-01-23 20:38:42 +0100

    chore: remove err vars (unused vars)

--- frontend/src/screens/exams/SummariesScreen.jsx
+++ frontend/src/screens/exams/SummariesScreen.jsx
@@ -4,10 +4,7 @@ import { Pagination } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import {
-  useGetDocumentsQuery,
-  useFilterDocumentsMutation,
-} from "../../slices/documentApiSlice";
+import { useGetDocumentsQuery, useFilterDocumentsMutation } from "../../slices/documentApiSlice";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import "./ExamsSCreen.css";
@@ -15,105 +12,111 @@ import ExamCard from "../../components/ExamCard";
 import Year from "../../components/year/filterRadio";
 
 const SummariesScreen = () => {
-  const [summaries, setSummaries] = useState();
+	const [summaries, setSummaries] = useState();
 
-  const { pageNumber, keyword } = useParams();
-  const { data, isLoading, isError } = useGetDocumentsQuery({
-    keyword,
-    pageNumber,
-    category: "summary",
-  });
+	const { pageNumber, keyword } = useParams();
+	const { data, isLoading, isError } = useGetDocumentsQuery({
+		keyword,
+		pageNumber,
+		category: "summary",
+	});
 
-  const [filterDocuments, { isLoading: loadingFiltered }, error] =
-    useFilterDocumentsMutation();
+	const [filterDocuments, { isLoading: loadingFiltered }] = useFilterDocumentsMutation();
 
-  const handleChange = async (event) => {
-    event.preventDefault();
+	const handleChange = async (event) => {
+		event.preventDefault();
 
-    if (!event.target.value) {
-      setSummaries(data);
-    } else {
-      try {
-        const newDocuments = await filterDocuments({
-          keyword,
-          pageNumber,
-          category: "summary",
-          year: event.target.value,
-        }).unwrap();
-        setSummaries(newDocuments);
-      } catch (error) {
-        toast.error(error?.data?.message || error.error);
-      }
-    }
-  };
+		if (!event.target.value) {
+			setSummaries(data);
+		} else {
+			try {
+				const newDocuments = await filterDocuments({
+					keyword,
+					pageNumber,
+					category: "summary",
+					year: event.target.value,
+				}).unwrap();
+				setSummaries(newDocuments);
+			} catch (error) {
+				toast.error(error?.data?.message || error.error);
+			}
+		}
+	};
 
-  useEffect(() => {
-    if (data) {
-      setSummaries(data);
-    }
-  }, [data]);
+	useEffect(() => {
+		if (data) {
+			setSummaries(data);
+		}
+	}, [data]);
 
-  return (
-    <>
-      <Row className="exams-row">
-        <Col className="filter-side" md={2}>
-          <Year handleChange={handleChange} />
-        </Col>
+	return (
+		<>
+			<Row className='exams-row'>
+				<Col
+					className='filter-side'
+					md={2}>
+					<Year handleChange={handleChange} />
+				</Col>
 
-        <Col
-          className="content-side"
-          style={{ backgroundColor: "#161616", minHeight: "100vh" }}
-          md={10}
-        >
-          <Row className="p-3 mt-3">
-            <Col>
-              <strong>Summaries</strong>
-            </Col>
-            {/*<Col>
+				<Col
+					className='content-side'
+					style={{ backgroundColor: "#161616", minHeight: "100vh" }}
+					md={10}>
+					<Row className='p-3 mt-3'>
+						<Col>
+							<strong>Summaries</strong>
+						</Col>
+						{/*<Col>
                 <strong>16 exams</strong>
               </Col> */}
-          </Row>
-          {loadingFiltered && <Loader />}
-          {isLoading ? (
-            <Loader />
-          ) : isError ? (
-            <Message variant="danger">
-              {isError?.data?.message || isError?.error}
-            </Message>
-          ) : (
-            <Row className="m-2">
-              {summaries?.categorizedDocs.map((document) => (
-                <Col key={document._id} sm={12} md={5} lg={4} xl={3}>
-                  <div className="card-container">
-                    <ExamCard document={document} className="m-3" />
-                  </div>
-                </Col>
-              ))}
+					</Row>
+					{loadingFiltered && <Loader />}
+					{isLoading ? (
+						<Loader />
+					) : isError ? (
+						<Message variant='danger'>{isError?.data?.message || isError?.error}</Message>
+					) : (
+						<Row className='m-2'>
+							{summaries?.categorizedDocs.map((document) => (
+								<Col
+									key={document._id}
+									sm={12}
+									md={5}
+									lg={4}
+									xl={3}>
+									<div className='card-container'>
+										<ExamCard
+											document={document}
+											className='m-3'
+										/>
+									</div>
+								</Col>
+							))}
 
-              {summaries?.categorizedDocs.length === 0 && (
-                <Row className="justify-content-center">
-                  <Col md={10}>
-                    <Message>no matches were found!</Message>
-                  </Col>
-                </Row>
-              )}
-            </Row>
-          )}
-          {summaries?.pages > 1 && (
-            <Pagination className="mx-auto my-2">
-              {[...Array(summaries?.pages).keys()].map((x) => (
-                <LinkContainer key={x + 1} to={`/summaries/page/${x + 1}`}>
-                  <Pagination.Item active={x + 1 === summaries?.page}>
-                    {x + 1}
-                  </Pagination.Item>
-                </LinkContainer>
-              ))}
-            </Pagination>
-          )}
-        </Col>
-      </Row>
-    </>
-  );
+							{summaries?.categorizedDocs.length === 0 && (
+								<Row className='justify-content-center'>
+									<Col md={10}>
+										<Message>no matches were found!</Message>
+									</Col>
+								</Row>
+							)}
+						</Row>
+					)}
+					{summaries?.pages > 1 && (
+						<Pagination className='mx-auto my-2'>
+							{[...Array(summaries?.pages).keys()].map((x) => (
+								<LinkContainer
+									key={x + 1}
+									to={`/summaries/page/${x + 1}`}>
+									<Pagination.Item active={x + 1 === summaries?.page}>{x + 1}</Pagination.Item>
+								</LinkContainer>
+							))}
+						</Pagination>
+					)}
+				</Col>
+			</Row>
+		</>
+	);
 };
 
 export default SummariesScreen;
