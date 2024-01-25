23c3780c610bb1100259ea20b5bc0d955dd8b28c
Aizen <acristyano22@gmail.com>
2024-01-25 10:37:54 +0100

    css: update colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "primary-green": "#75dab4",
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         playFair: ["Play Fair Display", "serif"],
