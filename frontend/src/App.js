42e0cc60fafa7aa0cdf9958e8c7984dc357b5c98
Aizen <acristyano22@gmail.com>
2024-01-25 11:10:50 +0100

    ui: change home sceen view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import { createContext } from "react";
-import 'react-toastify/dist/ReactToastify.css'
+import "react-toastify/dist/ReactToastify.css";
 
 export const RecoveryContext = createContext();
-
-
 
 const App = () => {
   return (
     <>
       <Header />
-      <main >
+      <main className='text-white'>
         <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
           <Outlet />
         </Container>
--- frontend/src/screens/homeScreen/HomeScreen.jsx
+++ frontend/src/screens/homeScreen/HomeScreen.jsx
@@ -1,34 +1,31 @@
-import React from "react";
-import { Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 import "./HomeScreen.css";
 
-
 const HomeScreen = () => {
-  
- 
-
   return (
-    <div className="home-container" >
-
-        <Col md={7} className="home-content">
-          <h1 className='mb-2 mt-3'>
-            Empower Your<br></br> Mind through<br></br> Medi
-            <span style={{ color: "#75dab4" }}>Q</span>uest
-          </h1>
-          <span style={{color:'#ebf4f5'}}>
-            With our user-friendly interface and <br /> comprehensive {" "}
-            curriculum, you'll<br /> embark  on a transformative journey
-          </span>
-          <Link to="/exams" className="mt-2">
-            <button type="button" className="start-btn">
-              START LEARNING {' '} <FaArrowRightLong color="black" />
-            </button>
-          </Link>
-        </Col>
-        <Col md={6}></Col>
-
+    <div className='home-container grid h-screen place-items-center text-center'>
+      <div className='flex flex-col items-center gap-20 max-md:gap-16'>
+        <h1 className='font-playFair text-[85px] font-[600] max-md:text-[40px]'>
+          Improve your skills with
+          <br /> Medi
+          <span className='font-playFair text-primary-green'>Q</span>uest
+        </h1>
+        <p className='max-w-[680px]'>
+          we are small team that aims to end your problems in searching for what
+          concerns you by providing a large amount of tools that will make you
+          advance in your future studies
+        </p>
+        <Link to='/exams'>
+          <button className='flex items-center gap-3 rounded-md bg-primary-green px-12 py-3 text-[1rem] font-semibold text-black duration-500 hover:scale-110 hover:bg-white hover:text-primary-green max-md:px-10 max-md:py-2 max-md:text-[15px]'>
+            START LEARNING
+            <FaArrowRightLong
+              color='black'
+              className='text-[27px] max-md:text-[20px]'
+            />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
