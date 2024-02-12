782fbf1b0bfbe967d5f26c5dc7761feeda334f27
Aizen <acristyano22@gmail.com>
2024-02-13 00:04:35 +0100

    chore (`Input`): relative fixes

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,11 +1,16 @@
 const Input = ({ handleChange, value, title, name, color }) => {
-    return (
-      <label className="sidebar-label-container">
-        <input onChange={handleChange} type="radio" value={value} name={name} />
-        <span className="checkmark" style={{ backgroundColor: color }}></span>
-        {title}
-      </label>
-    );
-  };
-  
-  export default Input;
+  return (
+    <label className='sidebar-label-container flex gap-4'>
+      <input
+        onChange={handleChange}
+        type='radio'
+        className='border-2 border-primary-green checked:bg-primary-green'
+        value={value}
+        name={name}
+      />
+      {title}
+    </label>
+  );
+};
+
+export default Input;
