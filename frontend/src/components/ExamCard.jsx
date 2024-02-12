11019cc6c2b574a9ddce46192c9ab0ec7aebe37e
Aizen <acristyano22@gmail.com>
2024-02-13 00:03:46 +0100

    ui(exam): change in `ExamCard` component + some relative changes in screen

--- frontend/src/components/ExamCard.jsx
+++ frontend/src/components/ExamCard.jsx
@@ -1,121 +1,107 @@
-import React, {useState,useEffect} from 'react'
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { Row, Col, Image } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Image } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { FaRegHeart } from "react-icons/fa6";
-import { FaComment } from "react-icons/fa";
-import { toast } from 'react-toastify';
-import { AiFillHeart } from "react-icons/ai"
-import Rating from './Rating'
-import '../screens/exams/ExamsSCreen.css' 
+import { toast } from "react-toastify";
+import { BsFillHeartFill, BsHeart } from "react-icons/bs";
+import "../screens/exams/ExamsSCreen.css";
+import { GoComment } from "react-icons/go";
 import {
-  useProfileMutation,
-  useGetFavListQuery,
   useGetUserProfileQuery,
   useAddToFavMutation,
 } from "../slices/usersApiSlice";
-import { useGetDocumentsQuery } from "../slices/documentApiSlice";
-import { setCredentials } from "../slices/authSlice";
- 
-const ExamCard = ({document}) => {
-  
-const [likes, setLikes] = useState(document.numLikes)
 
+const ExamCard = ({ document }) => {
+  const [likes, setLikes] = useState(document.numLikes);
+  const [color, setColor] = useState();
 
   const [addToFav] = useAddToFavMutation();
+  const { _id: documentId } = document;
 
-  const {_id: documentId} = document
-  const dispatch = useDispatch();
-  const { userInfoMediquest } = useSelector((state) => state.auth);
-
-
-  const { data: userProfile, refetch, isLoadingUserProfile } = useGetUserProfileQuery();
- 
+  const {
+    data: userProfile,
+    refetch,
+  } = useGetUserProfileQuery();
   
-  const [color, setColor]= useState()
-
-
   useEffect(() => {
-    const found = userProfile?.favourites.find(obj => {
+    const found = userProfile?.favourites.find((obj) => {
       return obj._id === document._id;
-    }) 
-    setColor(found ? "#fa3e5f": "#75dab4")
-  }, [userProfile?.favourites])
-
-
-
+    });
+    setColor(found ? "#fa3e5f" : "#75dab4");
+  }, [userProfile?.favourites]);
+  
   const submitHandler = async (e) => {
     e.preventDefault();
+
     try {
-      const res = await addToFav({_id: documentId,
+      const res = await addToFav({
+        _id: documentId,
         name: document.name,
         image: document.image,
         rating: document.rating,
         numReviews: document.numReviews,
-        numLikes: likes}).unwrap()
-        refetch()
-        
-   if(color === "#75dab4"){ setColor("#fa3e5f")
-    }else if (color === "#fa3e5f") {
-      setColor( "#75dab4")
-    }
-    
-    setLikes(res.likes)
+        numLikes: likes,
+      }).unwrap();
+      refetch();
+
+      if (color === "#75dab4") setColor("#fa3e5f");
+      else if (color === "#fa3e5f") setColor("#75dab4");
+      setLikes(res.likes);
       
-    } catch (err) {
+    } 
+    catch (err) {
       toast.error(err?.data?.message || err.error);
     }
   };
+
   return (
     <>
-     <div className="card">
-                <Link to={`/document/${document._id}`} className="product-image">
-                  <Image className="product-image" src={document.image} variant="top" />
-                </Link>
-                <Link to={`/document/${document._id}`}>
-                  <p className="product-title" style={{fontWeight: "bold", color:"white", marginBottom:"0"}}>{document.name}</p>
-                </Link>
-               
-                <Rating value={document.rating}  />
-                {/*text={`${document.numReviews} reviews`} */}
-                <div className="likes mt-1 px-2">
-                <FaComment   size={20}
-                    color="#75dab4"
-                    />
-                 <span
-                   style={{
-                    color: "#75dab4",
-                    fontSize: "24",
-                    marginLeft: "0.2rem",
-                    marginRight: "0.4rem",
-                 
-                  }}>
-                  {document.numReviews}
+      <div className='card font-sans rounded-md border border-white'>
+        <Link to={`/document/${document._id}`} className='product-image'>
+          <Image className='product-image' src={document.image} />
+        </Link>
+        <Link className="py-2.5" to={`/document/${document._id}`}>
+          <p
+            className='product-title font-normal px-1'
+            style={{  color: "white", marginBottom: "0" }}
+          >
+            {document.name}
+          </p>
+        </Link>
 
-                  </span>
-                  <AiFillHeart onClick={submitHandler}
-                    size={20}
-                    color={color}
-                    style={{  cursor:"pointer" }}
-                  />
-                  <span
-                   style={{
-                    color: `${color}`,
-                    fontSize: "24",
-                    marginLeft: "0.2rem",
-                    cursor:"pointer"
-                  }}>
-                    {likes}
-                  {/*document.numLikes*/}
-
-                  </span>
-                 
-                </div>
-              </div>
-    
+        {/* <Rating value={document.rating} /> */}
+        {/*text={`${document.numReviews} reviews`} */}
+        <div className='flex items-center  px-2'>
+          {color === '#75dab4'? <BsHeart
+            onClick={submitHandler}
+             size={27}
+              className='cursor-pointer text-primary-green' />
+               :
+                <BsFillHeartFill 
+                onClick={submitHandler}
+                 size={27}className='text-primary-green' /> }
+          
+          <span
+            className='text-primary-green cursor-pointer mx-1 color-primary-green'
+          >
+            {document.numReviews}
+          </span>
+          <GoComment size={29} color='#75dab4' />
+          <span
+            style={{
+              color: `${color}`,
+              fontSize: "24",
+              marginLeft: "0.2rem",
+              cursor: "pointer",
+            }}
+          >
+            {likes}
+            {/*document.numLikes*/}
+          </span>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ExamCard
+export default ExamCard;
--- frontend/src/screens/exams/ExamsScreen.jsx
+++ frontend/src/screens/exams/ExamsScreen.jsx
@@ -4,7 +4,10 @@ import { useParams } from "react-router-dom";
 import { Pagination } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { toast } from "react-toastify";
-import { useGetDocumentsQuery, useFilterDocumentsMutation } from "../../slices/documentApiSlice";
+import {
+  useGetDocumentsQuery,
+  useFilterDocumentsMutation,
+} from "../../slices/documentApiSlice";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import "./ExamsSCreen.css";
@@ -12,106 +15,99 @@ import ExamCard from "../../components/ExamCard";
 import Year from "../../components/year/filterRadio";
 
 const ExamsScreen = () => {
-	const [exams, setExams] = useState();
-	const { pageNumber, keyword } = useParams();
-	const { data, isLoading, isError } = useGetDocumentsQuery({
-		keyword,
-		pageNumber,
-		category: "exam",
-	});
+  const [exams, setExams] = useState();
+  const { pageNumber, keyword } = useParams();
+  const { data, isLoading, isError } = useGetDocumentsQuery({
+    keyword,
+    pageNumber,
+    category: "exam",
+  });
 
-	const [filterDocuments, { isLoading: loadingFiltered }] = useFilterDocumentsMutation();
+  const [filterDocuments, { isLoading: loadingFiltered }] =
+    useFilterDocumentsMutation();
 
-	const handleChange = async (event) => {
-		event.preventDefault();
-		if (!event.target.value) {
-			setExams(data);
-		} else {
-			try {
-				const newDocuments = await filterDocuments({
-					keyword,
-					pageNumber,
-					category: "exam",
-					year: event.target.value,
-				}).unwrap();
-				setExams(newDocuments);
-			} catch (error) {
-				toast.error(error?.data?.message || error.error);
-			}
-		}
-	};
+  const handleChange = async (event) => {
+    event.preventDefault();
+    if (!event.target.value) {
+      setExams(data);
+    } else {
+      try {
+        const newDocuments = await filterDocuments({
+          keyword,
+          pageNumber,
+          category: "exam",
+          year: event.target.value,
+        }).unwrap();
+        setExams(newDocuments);
+      } catch (error) {
+        toast.error(error?.data?.message || error.error);
+      }
+    }
+  };
 
-	useEffect(() => {
-		if (data) {
-			setExams(data);
-		}
-	}, [data]);
-	return (
-		<>
-			<Row className='exams-row'>
-				<Col
-					className='filter-side'
-					md={2}>
-					<Year handleChange={handleChange} />
-				</Col>
+  useEffect(() => {
+    if (data) {
+      setExams(data);
+    }
+  }, [data]);
+  return (
+    <>
+      <Row className='exams-row'>
+        <Col className='filter-side' md={2}>
+          <Year handleChange={handleChange} />
+        </Col>
 
-				<Col
-					className='content-side'
-					style={{ backgroundColor: "#161616", minHeight: "100vh" }}
-					md={10}>
-					<Row className='p-3 mt-3'>
-						<Col>
-							<strong>Exams</strong>
-						</Col>
-						<Col md={2}></Col>
-					</Row>{" "}
-					{loadingFiltered && <Loader />}
-					{/* .filter((item) => item.category === "exams") */}
-					{isLoading ? (
-						<Loader />
-					) : isError ? (
-						<Message variant='danger'>{isError?.data?.message || isError?.error}</Message>
-					) : (
-						<Row className='m-2'>
-							{exams?.categorizedDocs.map((document) => (
-								<Col
-									key={document._id}
-									sm={12}
-									md={5}
-									lg={4}
-									xl={3}>
-									<div className='card-container'>
-										<ExamCard
-											document={document}
-											className='m-3'
-										/>
-									</div>
-								</Col>
-							))}
-							{exams?.categorizedDocs.length === 0 && (
-								<Row className='justify-content-center'>
-									<Col md={10}>
-										<Message>no matches were found!</Message>
-									</Col>
-								</Row>
-							)}
-						</Row>
-					)}
-					{exams?.pages > 1 && (
-						<Pagination className='mx-auto my-2'>
-							{[...Array(exams?.pages).keys()].map((x) => (
-								<LinkContainer
-									key={x + 1}
-									to={`/exams/page/${x + 1}`}>
-									<Pagination.Item active={x + 1 === exams?.page}>{x + 1}</Pagination.Item>
-								</LinkContainer>
-							))}
-						</Pagination>
-					)}
-				</Col>
-			</Row>
-		</>
-	);
+        <Col
+          className='content-side'
+          style={{ backgroundColor: "#161616", minHeight: "100vh" }}
+          md={10}
+        >
+          <Row className='mt-3 p-3'>
+            <Col>
+              <strong>Exams</strong>
+            </Col>
+            <Col md={2}></Col>
+          </Row>{" "}
+          {loadingFiltered && <Loader />}
+          {isLoading ? (
+            <Loader />
+          ) : isError ? (
+            <Message variant='danger'>
+              {isError?.data?.message || isError?.error}
+            </Message>
+          ) : (
+            <Row className='m-2'>
+              {exams?.categorizedDocs.map((document) => (
+                <Col key={document._id} sm={12} md={5} lg={4} xl={3}>
+                  <div className='card-container'>
+                    <ExamCard document={document} className='m-3' />
+                  </div>
+                </Col>
+              ))}
+              {exams?.categorizedDocs.length === 0 && (
+                <Row className='justify-content-center'>
+                  <Col md={10}>
+                    <Message>no matches were found!</Message>
+                  </Col>
+                </Row>
+              )}
+            </Row>
+          )}
+          {exams?.pages > 1 && (
+            <Pagination className='mx-auto my-2'>
+              {[...Array(exams?.pages).keys()].map((x) => (
+                <LinkContainer key={x + 1} to={`/exams/page/${x + 1}`}>
+                  <Pagination.Item active={x + 1 === exams?.page}>
+                    {x + 1}
+                  </Pagination.Item>
+                </LinkContainer>
+              ))}
+            </Pagination>
+          )}
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ExamsScreen;
