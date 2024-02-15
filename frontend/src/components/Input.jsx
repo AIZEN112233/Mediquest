a0b855f3fdc1ddb78c5c9188327b10b8abc8923c
Aizen <acristyano22@gmail.com>
2024-02-15 19:12:42 +0100

    ui(filter); change radio ui

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,10 +1,10 @@
-const Input = ({ handleChange, value, title, name, color }) => {
+const Input = ({ handleChange, value, title, name }) => {
   return (
-    <label className='sidebar-label-container flex gap-4'>
+    <label className='sidebar-label-containe flex gap-4 items-center max-md:block max-md:mx-2 py-2 md:mx-4'>
       <input
         onChange={handleChange}
         type='radio'
-        className='border-2 border-primary-green checked:bg-primary-green'
+        className='border-2 w-4 h-4 rounded-sm border-primary-green checked:bg-primary-green appearance-none'
         value={value}
         name={name}
       />
@@ -14,3 +14,5 @@ const Input = ({ handleChange, value, title, name, color }) => {
 };
 
 export default Input;
+
+// FIXME: fix issue for pressing the checkmark twice to get activated
--- frontend/src/components/year/filterRadio.jsx
+++ frontend/src/components/year/filterRadio.jsx
@@ -1,60 +1,43 @@
 import "./filterRadio.css";
 import Input from "../../components/Input";
+import { useState } from "react";
 
 function Year({ handleChange }) {
+  const [isHidden, setIshidden] = useState(false);
   return (
-    <div className="ml-3 " style={{margin: "auto"}}>
-      <h2 className="sidebar-title">Year</h2>
- 
-      <div className=" filter-container" style={{margin: "auto"}}>
-        <label className="sidebar-label-container">
-          <input onChange={handleChange} type="radio" value="" name="test" />
-          <span className="checkmark"></span>All
-        </label>
-        <Input
-          handleChange={handleChange}
-          value="1"
-          title="1"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="2"
-          title="2"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="3"
-          title="3"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="4"
-          title="4"
-          name="test"
-        />
-        
-        <Input
-          handleChange={handleChange}
-          value="5"
-          title="5"
-          name="test"
-        />
-        <Input
-          handleChange={handleChange}
-          value="6"
-          title="6"
-          name="test"
-        />
-         <Input
-          handleChange={handleChange}
-          value="7"
-          title="7"
-          name="test"
-        />
-      </div>
+    <div className='m-auto ml-3'>
+      <h1 className='relative -translate-x-10 text-4xl font-semibold'>
+        Filter
+      </h1>
+      <h2
+        className='sidebar-title'
+        onClick={() => setIshidden((prev) => !prev)}
+      >
+        Year
+      </h2>
+
+      {isHidden && (
+        <div className=' filter-container' style={{ margin: "auto" }}>
+          <label className='sidebar-label-container'>
+            <input
+              onChange={handleChange}
+              className='mr-10 h-5 w-5 appearance-none rounded-sm border-2 border-primary-green checked:bg-primary-green'
+              type='radio'
+              value=''
+              name='test'
+            />
+            <span className='text-xl'>All</span>
+          </label>
+          <Input handleChange={handleChange} value='1' title='1' name='test' />
+          <Input handleChange={handleChange} value='2' title='2' name='test' />
+          <Input handleChange={handleChange} value='3' title='3' name='test' />
+          <Input handleChange={handleChange} value='4' title='4' name='test' />
+
+          <Input handleChange={handleChange} value='5' title='5' name='test' />
+          <Input handleChange={handleChange} value='6' title='6' name='test' />
+          <Input handleChange={handleChange} value='7' title='7' name='test' />
+        </div>
+      )}
     </div>
   );
 }
