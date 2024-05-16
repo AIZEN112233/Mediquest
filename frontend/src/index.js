be19247e3276dc8f1cf87b71cdc5daa748fd171c
kira kuin <bettaharabdelmadjid@gmail.com>
2024-05-16 06:37:34 -0700

    chore: unable strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -14,174 +19,85 @@ import PrivateRoute from "./components/PrivateRoute";
 import AdminRoute from "./components/AdminRoute";
 
 import {
-	CollectionScreen,
-	CoursesScreen,
-	DocumentListScreen,
-	DocumentScreen,
-	ExamsScreen,
-	HomeScreen,
-	LoginScreen,
-	OTPinput,
-	ProfileScreen,
-	RegisterScreen,
-	ResetPassword,
-	SearchScreen,
-	SummariesScreen,
-	UserEditScreen,
-	UserListScreen,
-	DocumentEditScreen,
+  CollectionScreen,
+  CoursesScreen,
+  DocumentListScreen,
+  DocumentScreen,
+  ExamsScreen,
+  HomeScreen,
+  LoginScreen,
+  OTPinput,
+  ProfileScreen,
+  RegisterScreen,
+  ResetPassword,
+  SearchScreen,
+  SummariesScreen,
+  UserEditScreen,
+  UserListScreen,
+  DocumentEditScreen,
 } from "./screens";
-// import reportWebVitals from "./reportWebVitals";
-// import HomeScreen from "./screens/homeScreen/HomeScreen";
-// import ExamsScreen from "./screens/exams/ExamsScreen";
-// import CoursesScreen from "./screens/exams/CoursesScreen";
-// import SummariesScreen from "./screens/exams/SummariesScreen";
-// import SearchScreen from "./screens/exams/SearchScreen";
-// import DocumentScreen from "./screens/DocumentScreen";
-// import LoginScreen from "./screens/loginScreen/LoginScreen";
-// import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
-// import OTPinput from "./screens/OTPinput";
-// import ResetPassword from "./screens/ResetPassword";
-// import CollectionScreen from "./screens/CollectionScreen";
-// import ProfileScreen from "./screens/profile/ProfileScreen";
-// import DocumentListScreen from "./screens/admin/DocumentListScreen";
-// import DocumentEditScreen from "./screens/admin/DocumentEditScreen";
-// import UserListScreen from "./screens/admin/UserListScreen";
-// import UserEditScreen from "./screens/admin/UserEditScreen";
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route
-			path='/'
-			element={<App />}>
-			<Route
-				index={true}
-				path='/'
-				element={<HomeScreen />}
-			/>
-			<Route
-				path='/exams'
-				element={<ExamsScreen />}
-			/>
-			<Route
-				path='/exams/page/:pageNumber'
-				element={<ExamsScreen />}
-			/>
-			<Route
-				path='/courses'
-				element={<CoursesScreen />}
-			/>
-			<Route
-				path='/courses/page/:pageNumber'
-				element={<CoursesScreen />}
-			/>
-			<Route
-				path='/summaries'
-				element={<SummariesScreen />}
-			/>
-			<Route
-				path='/summaries/page/:pageNumber'
-				element={<SummariesScreen />}
-			/>
-			<Route
-				path='/search/:keyword'
-				element={<SearchScreen />}
-			/>
-			<Route
-				path='/page/:pageNumber'
-				element={<SearchScreen />}
-			/>
-			<Route
-				path='/search/:keyword/page/:pageNumber'
-				element={<SearchScreen />}
-			/>
-			<Route
-				path='/document/:id'
-				element={<DocumentScreen />}
-			/>
-			<Route
-				path='/login'
-				element={<LoginScreen />}
-			/>
-			<Route
-				path='/register'
-				element={<RegisterScreen />}
-			/>
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/exams' element={<ExamsScreen />} />
+      <Route path='/exams/page/:pageNumber' element={<ExamsScreen />} />
+      <Route path='/courses' element={<CoursesScreen />} />
+      <Route path='/courses/page/:pageNumber' element={<CoursesScreen />} />
+      <Route path='/summaries' element={<SummariesScreen />} />
+      <Route path='/summaries/page/:pageNumber' element={<SummariesScreen />} />
+      <Route path='/search/:keyword' element={<SearchScreen />} />
+      <Route path='/page/:pageNumber' element={<SearchScreen />} />
+      <Route
+        path='/search/:keyword/page/:pageNumber'
+        element={<SearchScreen />}
+      />
+      <Route path='/document/:id' element={<DocumentScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
 
-			<Route
-				path=''
-				element={<OtpRoute />}>
-				<Route
-					path='/forgotpassword'
-					element={<OTPinput />}
-				/>
-			</Route>
+      <Route path='' element={<OtpRoute />}>
+        <Route path='/forgotpassword' element={<OTPinput />} />
+      </Route>
 
-			<Route
-				path=''
-				element={<ResetRoute />}>
-				<Route
-					path='/resetpassword'
-					element={<ResetPassword />}
-				/>
-			</Route>
+      <Route path='' element={<ResetRoute />}>
+        <Route path='/resetpassword' element={<ResetPassword />} />
+      </Route>
 
-			<Route element={<PrivateRoute />}>
-				<Route
-					path='/collection/:id'
-					element={<CollectionScreen />}
-				/>
-				<Route
-					path='/profile'
-					element={<ProfileScreen />}>
-					<Route
-						path='/profile/userinfo'
-						element={<ProfileScreen />}
-					/>
-					<Route
-						path='/profile/favourites'
-						element={<ProfileScreen />}
-					/>
-					<Route
-						path='/profile/collections'
-						element={<ProfileScreen />}
-					/>
-				</Route>
-			</Route>
+      <Route element={<PrivateRoute />}>
+        <Route path='/collection/:id' element={<CollectionScreen />} />
+        <Route path='/profile' element={<ProfileScreen />}>
+          <Route path='/profile/userinfo' element={<ProfileScreen />} />
+          <Route path='/profile/favourites' element={<ProfileScreen />} />
+          <Route path='/profile/collections' element={<ProfileScreen />} />
+        </Route>
+      </Route>
 
-			<Route element={<AdminRoute />}>
-				<Route
-					path='/admin/documentlist'
-					element={<DocumentListScreen />}
-				/>
-				<Route
-					path='/admin/documentlist/:pageNumber'
-					element={<DocumentListScreen />}
-				/>
-				<Route
-					path='/admin/document/:id/edit'
-					element={<DocumentEditScreen />}
-				/>
-				<Route
-					path='/admin/userlist'
-					element={<UserListScreen />}
-				/>
-				<Route
-					path='/admin/user/:id/edit'
-					element={<UserEditScreen />}
-				/>
-			</Route>
-		</Route>,
-	),
+      <Route element={<AdminRoute />}>
+        <Route path='/admin/documentlist' element={<DocumentListScreen />} />
+        <Route
+          path='/admin/documentlist/:pageNumber'
+          element={<DocumentListScreen />}
+        />
+        <Route
+          path='/admin/document/:id/edit'
+          element={<DocumentEditScreen />}
+        />
+        <Route path='/admin/userlist' element={<UserListScreen />} />
+        <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
+      </Route>
+    </Route>,
+  ),
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<HelmetProvider>
-			<Provider store={store}>
-				<RouterProvider router={router} />
-			</Provider>
-		</HelmetProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>,
 );
