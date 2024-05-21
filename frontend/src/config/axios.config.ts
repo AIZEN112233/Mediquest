bfa1d1e87ddd3570347eda2d9becb13f68f5c416
kira kuin <acristyano22@gmail.com>
2024-05-21 13:17:32 -0700

    feat: create public config for `axios`

---
+++ frontend/src/config/axios.config.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:5000/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
